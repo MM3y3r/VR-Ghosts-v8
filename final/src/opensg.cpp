d4962a584567c63b20ecfb3f7cffa3b12d67b3e5
Panagel <Panagel@web.de>
2017-09-22 20:26:14 +0200

    Revert "transformationKnots for ghost (5 for the 3 Axis)"
    This reverts commit 966a204902116b33a19a8bf3080a53b67014a2c8.

--- final/src/opensg.cpp
+++ final/src/opensg.cpp
@@ -61,11 +61,6 @@ OSG_USING_NAMESPACE // activate the OpenSG namespace
 //------------------------------------------------------------------------------
 SimpleSceneManagerRefPtr mgr; // the SimpleSceneManager to manage applications
 NodeRecPtr ghostTrans;
-NodeRecPtr ghostTrans2y;
-NodeRecPtr ghostTrans3y;
-NodeRecPtr ghostTrans4z;
-NodeRecPtr ghostTrans5z;
-
 NodeRecPtr boxTrans;
 NodeRecPtr boxTrans2;
 
