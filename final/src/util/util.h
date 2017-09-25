3a0f8a79d051070ad9db0b88e6d2506b31eb4c65
Panagel <Panagel@web.de>
2017-09-25 17:22:58 +0200

    cryptic compiling error
    vorerst auskommentiert

--- final/src/util/util.h
+++ final/src/util/util.h
@@ -1,43 +1,43 @@
-// Creators : Patrick Nagel & Maximilian Meyer
-
-
-#ifdef _MSC_VER
-# pragma warning(push)
-# pragma warning(disable:4251 4275 4290 4996 4231 4244)
-#endif
-
-#include <OpenSG/OSGGLUT.h>					//für GLUT
-#include <OpenSG/OSGConfig.h>				//für GLUT
-#include <OpenSG/OSGGLUTWindow.h>			//für GLUT
-#include <OpenSG/OSGSimpleSceneManager.h>	//für GLUT
-#include <OpenSG/OSGSimpleGeometry.h>
-#include <OpenSG/OSGComponentTransform.h>
-#include <OpenSG/OSGMaterialGroup.h>
-#include <OpenSG/OSGImage.h>
-#include <OpenSG/OSGSimpleTexturedMaterial.h>
-#include <OpenSG/OSGSceneFileHandler.h>
-#include <OpenSG/OSGTextureBackground.h>
-#include <OpenSG/OSGGradientBackground.h>
-#include <OpenSG/OSGPointLight.h>
-#include <OpenSG/OSGSpotLight.h>
-#include <OpenSG/OSGTransitPtr.h>
-#include <string> 
-#include <ctime>
-#include <OpenSG/OSGFieldContainerUtils.h> // für debugging
-#include <OpenSG/OSGIntersectAction.h>
-#include <OpenSG/OSGVolumeFunctions.h>
-#include <iostream>     //for using cout
-#include <stdlib.h>     //for using the function sleep
-#include <stdio.h>
-#include <time.h>
-#include <dos.h>
-
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-
-OSG_USING_NAMESPACE // activate the OpenSG namespace
-
-NodeRecPtr GhostSpawner(char name[256])
+//// Creators : Patrick Nagel & Maximilian Meyer
+//
+//
+//#ifdef _MSC_VER
+//# pragma warning(push)
+//# pragma warning(disable:4251 4275 4290 4996 4231 4244)
+//#endif
+//
+//#include <OpenSG/OSGGLUT.h>					//für GLUT
+//#include <OpenSG/OSGConfig.h>				//für GLUT
+//#include <OpenSG/OSGGLUTWindow.h>			//für GLUT
+//#include <OpenSG/OSGSimpleSceneManager.h>	//für GLUT
+//#include <OpenSG/OSGSimpleGeometry.h>
+//#include <OpenSG/OSGComponentTransform.h>
+//#include <OpenSG/OSGMaterialGroup.h>
+//#include <OpenSG/OSGImage.h>
+//#include <OpenSG/OSGSimpleTexturedMaterial.h>
+//#include <OpenSG/OSGSceneFileHandler.h>
+//#include <OpenSG/OSGTextureBackground.h>
+//#include <OpenSG/OSGGradientBackground.h>
+//#include <OpenSG/OSGPointLight.h>
+//#include <OpenSG/OSGSpotLight.h>
+//#include <OpenSG/OSGTransitPtr.h>
+//#include <string> 
+//#include <ctime>
+//#include <OpenSG/OSGFieldContainerUtils.h> // für debugging
+//#include <OpenSG/OSGIntersectAction.h>
+//#include <OpenSG/OSGVolumeFunctions.h>
+//#include <iostream>     //for using cout
+//#include <stdlib.h>     //for using the function sleep
+//#include <stdio.h>
+//#include <time.h>
+//#include <dos.h>
+//
+//#ifdef _WIN32
+//#include <Windows.h>
+//#else
+//#include <unistd.h>
+//#endif
+//
+//OSG_USING_NAMESPACE // activate the OpenSG namespace
+//
+//NodeRecPtr GhostSpawner(char name[256])
